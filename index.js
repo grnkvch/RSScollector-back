376cb122a965cfbf426373f075843d56d63a819d
Valentin Grinkevich <valentin_grinkevich@epbyminw8934.minsk.epam.com>
2019-09-02 07:38:34 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const request = require('request');
+const Parser = require('rss-parser');
+const parser = new Parser();
 
 const app = express();
 
@@ -9,22 +11,48 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('', (req, res) => {
-  // console.log(req.query);
-  // console.log(req);
-  const url = req.query.source === "onliner" ? 'https://www.onliner.by/feed' : 'https://news.tut.by/rss'
-  request(
-    { url },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
+const asyncRequest = (url) => {
+  return new Promise(resolve => {
+    request(
+      { url },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: 'error', message: err.message });
+        }
+        // console.log('body', body);
+        // console.log('response', response);
+        // res.send(body);
+        parser.parseString(body).then((parsed) => {
+          console.log(parsed);
+          resolve(parsed);
+        });
       }
-      // console.log('body', body);
-      // console.log('response', response);
-      res.send(body);
-      // res.json(JSON.parse(body));
-    }
-  )
+    )
+  });
+}
+
+app.get('', async (req, res) => {
+  let url;
+  console.log(req.query);
+  switch (req.query.source) {
+    case '1':
+      url = 'https://www.onliner.by/feed';
+      break;
+    case '2':
+      url = 'https://news.tut.by/rss';
+      break;
+    case '3':
+      url = 'https://naviny.by/rss/alls.xml';
+      break;
+
+    default:
+      url = 'https://www.onliner.by/feed';
+      break;
+  }
+  const pars1 = await asyncRequest('https://www.onliner.by/feed');
+  const pars2 = await asyncRequest('https://news.tut.by/rss');
+  const pars3 = await asyncRequest('https://naviny.by/rss/alls.xml');
+  res.send({ pars1, pars2, pars3 });
 });
 
 const PORT = process.env.PORT || 3000;
